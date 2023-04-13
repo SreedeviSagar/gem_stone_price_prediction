685047029884db9d90245512a30f4d8baccda663
Sreedevi Sagar <115715763+SreedeviSagar@users.noreply.github.com>
2023-04-13 21:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-
 application=Flask(__name__)
 
-app=application
 
+
+app=application
 
 ## Route for a home page
 
